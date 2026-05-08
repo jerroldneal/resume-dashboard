0792ec62cca2545b51c68326abe08ab8736336bc
jerroldneal <jerrold.neal.bitcoin@gmail.com>
2026-05-07 22:03:54 -0700

    Add dark mode initialization by default (DockHand requirement)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Enable dark mode by default (DockHand requirement)
+document.documentElement.classList.add('dark')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
